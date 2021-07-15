52a7a8afbb14a6b0461a1144921ba0fb4a538434
Ryan J. McDonough <ryan@damnhandy.com>
2021-07-15 07:55:24 -0400

    Fixing Spring Cloud AWS config

--- springboot-app/src/main/java/com/example/awscloud/config/JerseyConfig.java
+++ springboot-app/src/main/java/com/example/awscloud/config/JerseyConfig.java
@@ -1,17 +1,10 @@
 package com.example.awscloud.config;
 
 import com.example.awscloud.resources.CarResource;
-import java.util.Arrays;
-import java.util.stream.StreamSupport;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.core.env.EnumerablePropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.MutablePropertySources;
 
 /**
  * <p>
@@ -23,10 +16,7 @@ public class JerseyConfig extends ResourceConfig {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(JerseyConfig.class);
 
-  Environment environment;
-
-  public JerseyConfig(Environment environment) {
-    this.environment = environment;
+  public JerseyConfig() {
     register(CarResource.class);
   }
 }
